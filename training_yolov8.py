00150cf1679c1169daea44fee0aa5cb8e3e03c95
oscarwzt <lsklsklsk81@gmail.com>
2023-12-31 11:47:19 -0500

    uploaded new weights

--- training_yolov8.py
+++ training_yolov8.py
@@ -3,12 +3,12 @@ from ultralytics import YOLO
 model = YOLO("yolov8l.pt")
 # model = YOLO("runs/detect/train7/weights/last.pt")
 model.train(data = "basketballDetection-27/data.yaml", 
-            epochs = 350, 
+            epochs = 500, 
             batch = 64, 
             # imgsz = 640, 
-            save_period=20,
+            save_period=50,
             # lr0 = 0.001,
-            lrf = 0.005,
+            lrf = 0.01,
             workers = 10,
             device = [0, 1],
             patience = 30,
